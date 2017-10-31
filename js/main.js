447ce3a467d39a5da7fda8c593c673cf796dc8c3
MAKIO135 <makio135@gmail.com>
2017-10-31 12:04:06 +0100

    reorganize code

--- js/main.js
+++ js/main.js
@@ -1,43 +1,3 @@
-// // UI elements
-// let deviceNameLabel = document.getElementById('device-name');
-// let connectButton = document.getElementById('connect');
-// let disconnectButton = document.getElementById('disconnect');
-// let terminalContainer = document.getElementById('terminal');
-// let sendForm = document.getElementById('send-form');
-// let inputField = document.getElementById('input');
-//
-// // Helpers
-// let defaultDeviceName = 'Terminal';
-// let terminalAutoScrollingLimit = terminalContainer.offsetHeight / 2;
-// let isTerminalAutoScrolling = true;
-//
-// function scrollElement(element) {
-//   let scrollTop = element.scrollHeight - element.offsetHeight;
-//
-//   if (scrollTop > 0) {
-//     element.scrollTop = scrollTop;
-//   }
-// }
-//
-// function logToTerminal(message, type = '') {
-//   let element = '<div' + (type ? ' class="' + type + '"' : '') + '>' + message +
-//       '</div>';
-//
-//   terminalContainer.insertAdjacentHTML('beforeend', element);
-//
-//   if (isTerminalAutoScrolling) {
-//     scrollElement(terminalContainer);
-//   }
-// }
-
-let connectButton = document.getElementById('connect');
-let disconnectButton = document.getElementById('disconnect');
-
-document.querySelectorAll( '.cmd' ).forEach( d => d.addEventListener( 'click', e => {
-    console.log( d.innerText );
-    terminal.send( d.innerText );
-} ) );
-
 // Obtain configured Bluetooth Terminal instance
 let terminal = new BluetoothTerminal();
 
@@ -56,42 +16,28 @@ terminal._log = function(...messages) {
     });
 };
 
-// Implement own send function to log outcoming data to the terminal
-function send(data) {
-    terminal.send(data).then(() => console.log(data, 'out')).catch(error => console.log(error));
-}
-
 // Bind event listeners to the UI elements
+let connectButton = document.getElementById('connect');
 connectButton.addEventListener('click', function() {
     terminal.connect().then(() => {
-        // deviceNameLabel.textContent = terminal.getDeviceName()
-        //     ? terminal.getDeviceName()
-        //     : defaultDeviceName;
         console.log( `connected to ${terminal.getDeviceName()}`)
     });
 });
 
+let disconnectButton = document.getElementById('disconnect');
 disconnectButton.addEventListener('click', function() {
     terminal.disconnect();
     console.log( 'disconnected' );
-    // deviceNameLabel.textContent = defaultDeviceName;
 });
 
-// sendForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//
-//     send(inputField.value);
-//
-//     inputField.value = '';
-//     inputField.focus();
-// });
-
-// Switch terminal auto scrolling if it scrolls out of bottom
-// terminalContainer.addEventListener('scroll', function() {
-//     let scrollTopOffset = terminalContainer.scrollHeight - terminalContainer.offsetHeight - terminalAutoScrollingLimit;
-//
-//     isTerminalAutoScrolling = (scrollTopOffset < terminalContainer.scrollTop);
-// });
+// Implement own send function to log outcoming data to the terminal
+function send(data) {
+    terminal.send(data).then(() => console.log(data, 'out')).catch(error => console.log(error));
+}
+document.querySelectorAll( '.cmd' ).forEach( d => d.addEventListener( 'click', e => {
+    console.log( d.dataset.cmd );
+    send( d.dataset.cmd );
+} ) );
 
 addEventListener('load', e => {
     mobileConsole.init();
